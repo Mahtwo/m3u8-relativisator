32f082ad0ec9a65e2057dbf1837a54f86d82818d
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2022-03-23 00:39:59 +0100

    Use FileSavePicker on Windows

--- m3u8-relativisator/m3u8-relativisator.UWP/MainPage.xaml.cs
+++ m3u8-relativisator/m3u8-relativisator.UWP/MainPage.xaml.cs
@@ -25,13 +25,49 @@ namespace m3u8_relativisator.UWP
         //UWP specific code for saving the file
         public async Task<string> SaveAs(string temporaryFilePath, string fileName, string originalFilePath)
         {
-            await Share.RequestAsync(new ShareFileRequest
+            //Specify options for saving as
+            FileSavePicker savePicker = new FileSavePicker
             {
-                Title = fileName,
-                File = new ShareFile(temporaryFilePath)
-            });
+                //Default to the filename of the original file
+                SuggestedFileName = fileName
+            };
+            //Dropdown of file types the user can save the file as
+            savePicker.FileTypeChoices.Add("Playlist file", new List<string>() {".m3u8", ".m3u"});
+            savePicker.FileTypeChoices.Add("All types", new List<string>() {"."});  //A wildcard throws an exception
 
-            return null;
+            //Open the save file dialog
+            StorageFile file = await savePicker.PickSaveFileAsync();
+
+            if (file != null)
+            {
+                //Prevent updates to the remote version of the file until we finish making changes and call CompleteUpdatesAsync.
+                CachedFileManager.DeferUpdates(file);
+
+                //Copy 
+                using (Stream fileStreamW = await file.OpenStreamForWriteAsync())
+                {
+                    using (Stream temporaryFileStream = new System.IO.FileStream(temporaryFilePath, FileMode.Open))
+                    {
+                        await temporaryFileStream.CopyToAsync(fileStreamW);
+                    }
+                }
+
+                //Let Windows know that we're finished changing the file so the other app can update the remote version of the file.
+                //Completing updates may require Windows to ask for user input.
+                FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
+                if (status == FileUpdateStatus.Complete)
+                {
+                    return "File saved as \"" + file.Name + '"';
+                }
+                else
+                {
+                    return "Couldn't save the file";
+                }
+            }
+            else
+            {
+                return "Saving cancelled";
+            }
         }
     }
 
